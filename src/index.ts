b8843c876f95f97512c40ec6becef9f817ec16e3
Brian Jemilo II <jemiloii@gmail.com>
2025-11-13 15:17:30 -0600

    Refactor exports in index.ts for SSLConfig type to fix build warning

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ export { InMemoryEventStore } from "./InMemoryEventStore.js";
 export { proxyServer } from "./proxyServer.js";
 export type { CorsOptions } from "./startHTTPServer.js";
 export { startHTTPServer } from "./startHTTPServer.js";
-export { SSLConfig, startHTTPSServer } from "./startHTTPSServer.js";
+export type { SSLConfig } from "./startHTTPSServer.js";
+export { startHTTPSServer } from "./startHTTPSServer.js";
 export { ServerType, startStdioServer } from "./startStdioServer.js";
 export { tapTransport } from "./tapTransport.js";
